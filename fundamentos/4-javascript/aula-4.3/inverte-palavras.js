6b5153ffe2dd161fe88c83e7b97c730136626b59
vinicius-lamberti <viniciusantunes2000@gmail.com>
2022-09-26 19:40:54 -0300

    Correção exercício inverte-palavras

--- fundamentos/4-javascript/aula-4.3/inverte-palavras.js
+++ fundamentos/4-javascript/aula-4.3/inverte-palavras.js
@@ -1,10 +1,10 @@
 //Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
 let word = "banana";
-let inv = "";
+let inv = [];
 
-for (i = 0; i < word.length; i += 1) {
-  inv.unshift(word[i]);
+for (i = word.length -1; i >= 0; i -= 1) {
+  inv.push(word[i]);
 }
 
 console.log(inv);
